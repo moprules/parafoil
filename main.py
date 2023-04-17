1a417b79b8374286eb8efd8894fd9665484fab30
moprules <rav-navini-gego-cutropal@yandex.ru>
2023-04-17 21:20:40 +0300

    del trash

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from parafoil import PFSim
 def main():
     lander = PFSim("data/space_rider.yaml")
     lander.start()
-    print(lander.state["force_payload_bodyframe"])
-    print(lander.state["moment_payload_bodyframe"])
 
 
 if __name__ == "__main__":
